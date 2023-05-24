6cee98b7d62e9f0bf95106160682c1d5d59ed2fd
Matt Quayle <mattgq2@gmail.com>
2023-05-24 11:20:26 -0600

    Update index.js

--- index.js
+++ index.js
@@ -34,8 +34,8 @@ require('./passport');
 app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
-//mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //MAIN PAGE
 app.get('/', (req, res) => {
